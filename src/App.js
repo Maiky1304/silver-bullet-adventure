d1514506264c330cd2f3783d44ae48cd913bc916
maiky perlee <maikyperlee@icloud.com>
2022-01-11 13:26:53 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,147 @@
-import logo from './logo.svg';
-import './App.css';
+import { useRef } from "react";
+import styled from "styled-components";
+import useImage from "./context/ImageContext";
+
+const images = [
+  require("./images/0.jpg"),
+  require("./images/1.jpg"),
+  require("./images/2.jpg"),
+  require("./images/3.jpg"),
+  require("./images/4.jpg"),
+  require("./images/5.jpg"),
+  require("./images/6.jpg"),
+  require("./images/7.jpg"),
+  require("./images/8.jpg"),
+  require("./images/9.jpg"),
+  require("./images/10.jpg"),
+  require("./images/11.jpg"),
+];
+
+const Container = styled.div`
+  background-color: #cdcdcd;
+  min-height: 100vh;
+
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+
+  font-family: "Spline Sans", sans-serif;
+`;
+
+const Image = styled.img`
+  border-top-left-radius: 25px;
+  border-bottom-left-radius: 25px;
+  transition: 0.8s all;
+
+  &:hover {
+    border-top-left-radius: 15px;
+    border-bottom-left-radius: 15px;
+  }
+`;
+
+const Card = styled.div`
+  background-color: white;
+  width: 900px;
+  height: 600px;
+  border-radius: 25px;
+  box-shadow: 0px 0px 20px 5px #f2f2f2;
+
+  display: flex;
+`;
+
+const ToolsSection = styled.div`
+  padding: 25px;
+`;
+
+const ToolsHeading = styled.h1`
+  position: relative;
+  &::after {
+    content: "";
+    width: 50px;
+    height: 5px;
+    position: absolute;
+    bottom: -10px;
+    left: 0;
+    background-color: black;
+    border-radius: 25px;
+  }
+`;
+
+const ToolsButtons = styled.div`
+  padding-top: 35px;
+  display: flex;
+  flex-wrap: wrap;
+  gap: 25px;
+`;
+
+const ToolsButton = styled.button`
+  width: 35px;
+  height: 35px;
+  background-color: #bdc3c7;
+  border: none;
+  border-radius: 25px;
+  font-weight: 700;
+  font-size: 20px;
+  color: #303841;
+  transition: 0.8s all;
+
+  &:hover {
+    cursor: pointer;
+    transform: scale(1.08);
+  }
+
+  &.current {
+    background-color: #113f67;
+    color: #fff;
+  }
+`;
+
+const Footer = styled.div`
+  margin-top: 25px;
+  opacity: 0.5;
+`;
 
 function App() {
+  const [image, setImage] = useImage();
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <Card>
+        <Image height="600px" src={images[image]} />
+        <ToolsSection>
+          <ToolsHeading>Silver Bullet Adventure</ToolsHeading>
+          <ToolsButtons>
+            {images
+              .map((_v, index) => index)
+              .map((num) =>
+                image === num ? (
+                  <ToolsButton
+                    className="current"
+                    onClick={() => setImage(num)}
+                  >
+                    {num}
+                  </ToolsButton>
+                ) : (
+                  <ToolsButton onClick={() => setImage(num)}>{num}</ToolsButton>
+                )
+              )}
+          </ToolsButtons>
+        </ToolsSection>
+      </Card>
+      <Footer>
+        <span>
+          Maiky Perlee - SD1C &bull;{" "}
+          <a
+            target="_blank"
+            rel="noreferrer"
+            href="https://github.com/Maiky1304"
+          >
+            GitHub
+          </a>
+        </span>
+      </Footer>
+    </Container>
   );
 }
 
---
+++ src/context/ImageContext.jsx
@@ -0,0 +1,15 @@
+import { createContext, useContext, useState } from "react";
+
+const ImageContext = createContext(0);
+
+const ImageProvider = ({ children }) => {
+  const state = useState(0);
+  return (
+    <ImageContext.Provider value={state}>{children}</ImageContext.Provider>
+  );
+};
+
+export { ImageProvider };
+export default function useImage() {
+  return useContext(ImageContext);
+}
